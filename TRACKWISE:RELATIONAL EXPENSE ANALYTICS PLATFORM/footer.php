491c350e23030d7a47ba06353dece0adf30e2138
vtu26076-manaswini <vtu26076@veltech.edu.in>
2026-04-06 12:12:25 +0530

    Create footer.php

---
+++ TRACKWISE:RELATIONAL EXPENSE ANALYTICS PLATFORM/footer.php
@@ -0,0 +1,14 @@
+</div>
+
+<footer class="bg-dark text-white text-center p-3 mt-auto">
+
+<p class="mb-0">
+TrackWise Expense System © 2026
+</p>
+
+</footer>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+
+</body>
+</html>
