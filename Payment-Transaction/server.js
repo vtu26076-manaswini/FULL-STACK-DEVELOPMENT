9a5ddcc5d826d281bebb98d3e65439c0500d4f1b
vtu26076-manaswini <vtu26076@veltech.edu.in>
2026-04-06 11:45:04 +0530

    Create server.js

---
+++ Payment-Transaction/server.js
@@ -0,0 +1,10 @@
+
+app.get("/orders",(req,res)=>{
+    const q=`
+    SELECT c.name,p.name AS product,o.qty,p.price
+    FROM orders o
+    JOIN customers c ON o.cid=c.id
+    JOIN products p ON o.pid=p.id
+    `;
+    db.query(q,(e,r)=>res.json(r));
+});
