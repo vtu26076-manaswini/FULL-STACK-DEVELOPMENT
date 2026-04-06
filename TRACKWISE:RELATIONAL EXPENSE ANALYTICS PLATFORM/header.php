c33ebe63caa1047fe917f8896587d19bd3580424
vtu26076-manaswini <vtu26076@veltech.edu.in>
2026-04-06 12:12:55 +0530

    Create header.php

---
+++ TRACKWISE:RELATIONAL EXPENSE ANALYTICS PLATFORM/header.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>TrackWise</title>
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+
+<style>
+body{
+    background:#f4f6f9;
+}
+
+.card-hover:hover{
+    transform: scale(1.03);
+    transition:0.3s;
+}
+</style>
+
+</head>
+
+<body class="d-flex flex-column min-vh-100">
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary shadow">
+<div class="container">
+
+<a class="navbar-brand fw-bold" href="index.php">
+TrackWise
+</a>
+
+<button class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#nav">
+<span class="navbar-toggler-icon"></span>
+</button>
+
+<div class="collapse navbar-collapse" id="nav">
+
+<ul class="navbar-nav ms-auto">
+
+<li class="nav-item">
+<a class="nav-link" href="index.php">Home</a>
+</li>
+
+<li class="nav-item">
+<a class="nav-link" href="users.php">Users</a>
+</li>
+
+<li class="nav-item">
+<a class="nav-link" href="expenses.php">Expenses</a>
+</li>
+
+<li class="nav-item">
+<a class="nav-link" href="analytics.php">Analytics</a>
+</li>
+
+</ul>
+
+</div>
+
+</div>
+</nav>
+
+<!-- Main Content -->
+<div class="container mt-4 flex-grow-1">
